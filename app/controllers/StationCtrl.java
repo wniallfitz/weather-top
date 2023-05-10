3ad50fff749e6ee962e37f7762d6a007b4276427
Niall Fitzgerald <w.niall.fitzgerald@gmail.com>
2023-05-10 22:51:10 +0100

    Add station & readings forms now working. Cant get "folder" icon working to expand dashboard view into individual stations.

--- app/controllers/StationCtrl.java
+++ app/controllers/StationCtrl.java
@@ -19,14 +19,22 @@ public class StationCtrl extends Controller
 
     }
 
-    public static void addReading(Long id, int code, float temperature, float windSpeed, int windDirection, int pressure)
+    public static void addReading(Long id, int code, float temperature, float windSpeed, int pressure, float windDirection)
     {
-        Reading reading = new Reading(code, temperature, windSpeed, windDirection, pressure);
+        Reading reading = new Reading(code, temperature, windSpeed, pressure, windDirection);
         Station station = Station.findById(id);
         station.readings.add(reading);
         station.save();
         redirect ("/stations/" + id);
     }
 
+//    public static void addSong(Long id, String title, String artist, int duration)
+//    {
+//        Song song = new Song(title, artist, duration);
+//        Playlist playlist = Playlist.findById(id);
+//        playlist.songs.add(song);
+//        playlist.save();
+//        redirect ("/playlists/" + id);
+//    }
 
 }
