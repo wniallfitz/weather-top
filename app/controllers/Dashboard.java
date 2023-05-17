768b8e76aa5eff2639084b5df222a87c42cc81cd
Niall Fitzgerald <w.niall.fitzgerald@gmail.com>
2023-05-17 21:13:54 +0100

    Release 2 complete

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import models.Member;
 import models.Station;
 import models.Reading;
 import play.Logger;
@@ -12,17 +13,23 @@ public class Dashboard extends Controller
 {
   public static void index()
   {
-    Logger.info("Rendering Admin");
-
-    List<Station> stations = Station.findAll();
+    Logger.info("Rendering Dashboard");
+    Member member = Accounts.getLoggedInMember();
+    List<Station> stations = member.stations;
+//    List<Station> stations = Station.findAll();
     render ("dashboard.html", stations);
   }
 
   public static void addStation (String name, double latitude, double longitude)
   {
-    Station station = new Station (name, latitude, longitude);
     Logger.info ("Adding a new station called " + name);
-    station.save();
+    Member member = Accounts.getLoggedInMember();
+    Station station = new Station (name, latitude, longitude);
+    member.stations.add(station);
+    member.save();
+//    station.save();
     redirect ("/dashboard");
   }
+  
 }
+
